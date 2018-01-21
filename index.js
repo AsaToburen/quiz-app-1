9a39dd5362c434339eb7cc8c27ba2b6251d03351
Asa Toburen <atopettit@gmail.com>
2018-01-21 08:53:34 -0700

    a11y-fixes

--- index.js
+++ index.js
@@ -78,8 +78,8 @@ function scoreQuiz() {
 
 function showAnswerModal(val) {
   let gif = val
-    ? `<div style="width:100%;height:0;padding-bottom:80%;position:relative;"><iframe src="https://giphy.com/embed/6yKquSnGwI5Ak" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/90s-high-five-6yKquSnGwI5Ak">via GIPHY</a></p>`
-    : `<div style="width:100%;height:0;padding-bottom:80%;position:relative;"><iframe src="https://giphy.com/embed/HgrgVb5paX768" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/cereal-HgrgVb5paX768">via GIPHY</a></p>`;
+    ? `<div style="width:100%;height:0;padding-bottom:80%;position:relative;"><iframe title="correct-gif" src="https://giphy.com/embed/6yKquSnGwI5Ak" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>`
+    : `<div style="width:100%;height:0;padding-bottom:80%;position:relative;"><iframe title="incorrect-gif" src="https://giphy.com/embed/HgrgVb5paX768" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>`;
 
   let modalHTML = `<div class="feedback">
         ${gif}
